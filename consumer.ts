c42e511aeafb7a87132a8e6eb061707cfcca5216
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-03-10 22:53:58 +0100

    added the redisInSubscribedState consumer argument

--- consumer.ts
+++ consumer.ts
@@ -1,35 +1,37 @@
 import { Redis } from "ioredis";
 import BaseService from "./baseService";
+import { QueueData } from "./types";
 
-export declare type fArgVoid = (object: Object) => void;
+export declare type fArgVoid = (object: string) => void;
 
 export default class Consumer extends BaseService {
     
-    constructor(queueName: string, redis: Redis) {
-        super(queueName, redis);
-    }
-
-    async subscribe(callback: fArgVoid) {
-        
-        await this._redis.subscribe(this._notificationQueue);
-        this._redis.on(this._notificationQueue, this.handler(callback));
-        
-        
-    }
-
-
+    redisInSubscribedState: Redis;
     
+    constructor(queueName: string, redis: Redis, redisInSubscribedState: Redis) {
+        super(queueName, redis);
+
+        this.redisInSubscribedState = redisInSubscribedState;
+    }
+
+    async subscribe(callback: fArgVoid) {        
+        try {
+            await this.redisInSubscribedState.subscribe(this._notificationQueue);
+            this.redisInSubscribedState.on("message", this.handler(callback));    
+        } catch (error) {
+            console.log(error);
+            throw(error);
+        }
+    }
 
     private handler(callback: fArgVoid): (...args: any[]) => void {
         return async () => {
 
             const jobId = await this._redis.rpoplpush(this._publishedQueue, this._processingQueue);
             const dataKey = this.getDataKeyByJobId(jobId);
-            
-            const obj = await this._redis.hgetall(dataKey);
+            const obj = <QueueData><unknown>(await this._redis.hgetall(dataKey));
 
-            //const obj = {};
-            callback(obj);
+            callback(obj.payload);
         };
     }
 }
--- index.ts
+++ index.ts
@@ -1,23 +1,20 @@
 import Redis from "ioredis";
 import Producer from "./producer";
-// import Consumer from "./consumer";
+import Consumer from "./consumer";
 
-const redis = new Redis();
 
-const producer = new Producer("test", redis);
+const producer = new Producer("test", new Redis());
 const obj = {
-    id: 1, 
+    userId: "123-ER-09", 
     result: "success"
 };
 
-//const consumer = new Consumer("test", redis);
+const consumer = new Consumer("test", new Redis(), new Redis());
 
-// consumer.subscribe((obj) => {
-//     console.log(obj);
-// });
-
-console.log("Adding the object in to the queue to process...");
-producer.add(JSON.stringify(obj))
+consumer.subscribe((objAsStr) => {
+    console.table(JSON.parse(objAsStr));
+}).then(() => {
+    producer.add(JSON.stringify(obj))
     .then(() => console.log("OK"))
     .catch((err) => console.log("Something went wrong: " + err));
-//console.log("The object has been added into the queue.");
+});
