2fea0d8ee706af789307b75db847da59f7da7d13
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-03-12 22:03:13 +0100

    added removing a processed item from the queue

--- consumer.ts
+++ consumer.ts
@@ -1,6 +1,7 @@
 import { Redis } from "ioredis";
 import BaseService from "./baseService";
 import { QueueData } from "./types";
+import { nameof } from "./utils";
 
 export declare type fArgVoid = (object: string) => void;
 
@@ -8,12 +9,20 @@ export default class Consumer extends BaseService {
     
     redisInSubscribedState: Redis;
     
+    /**
+     * Creates an instance of the consumer.
+     * Once the client enters the subscribed state it is not supposed to issue any other commands, 
+     * a second ready client is required.
+    */
     constructor(queueName: string, redis: Redis, redisInSubscribedState: Redis) {
         super(queueName, redis);
 
         this.redisInSubscribedState = redisInSubscribedState;
     }
 
+    /**
+     * Subscribes the client to the specified channel.
+     */
     async subscribe(callback: fArgVoid) {        
         try {
             await this.redisInSubscribedState.subscribe(this._notificationQueue);
@@ -22,16 +31,22 @@ export default class Consumer extends BaseService {
             console.log(error);
             throw(error);
         }
-    }
+    }   
 
     private handler(callback: fArgVoid): (...args: any[]) => void {
         return async () => {
 
-            const jobId = await this._redis.rpoplpush(this._publishedQueue, this._processingQueue);
-            const dataKey = this.getDataKeyByJobId(jobId);
+            const processedJobId = await this._redis.rpoplpush(this._publishedQueue, this._processingQueue);
+            const dataKey = this.getDataKeyByJobId(processedJobId);
             const obj = <QueueData><unknown>(await this._redis.hgetall(dataKey));
 
             callback(obj.payload);
+
+            await this._redis
+                    .multi()
+                    .hdel(dataKey, nameof<QueueData>("createdDt"), nameof<QueueData>("payload"))
+                    .lrem(this._processingQueue, 0, processedJobId)                    
+                    .exec();
         };
     }
 }
