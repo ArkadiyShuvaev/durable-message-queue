acdb115781f07754da5c53833f3b9fcbe62d0c8c
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-03-29 22:24:53 +0200

    added index.ts

--- index.ts
+++ index.ts
@@ -7,33 +7,42 @@ import Builder from "./src/builder";
 const queueName = "test";
 
 const queueManager = Builder.createQueueManager(queueName);
-//queueManager.start();
+queueManager.start();
 
 const producer = Builder.createProducer(queueName);
 const obj = {
-    dummyString: new Date().toISOString(), 
+    dummyString: new Date().toISOString(),
     result: "success"
 };
 
 producer
     .send(JSON.stringify(obj))
-    .then(() => console.log("The producer sent a message to queue."))
-    .catch((err) =>
-        console.log(`Something went wrong and a message could not sent to the queue "${queueName}": ${err}`));
-    
-producer
-    .send(JSON.stringify(obj))
-    .then(() => console.log("The producer sent a message to queue."))
+    //.then(() => console.log("The producer sent a message to queue."))
     .catch((err) =>
         console.log(`Something went wrong and a message could not sent to the queue "${queueName}": ${err}`));
 
 const consumer = Builder.createConsumer(queueName, { processingTimeout: 300 });
 
-consumer.subscribe((objAsStr) => {
-    console.table(JSON.parse(objAsStr));    
+consumer.subscribe((message) => {
+    //console.table(message);
 }).then(() => {
     producer.send(JSON.stringify(obj))
-    .then(() => console.log("The producer sent a message to queue."))
+    // .then(() => console.log("The producer sent a message to queue."))
     .catch((err) =>
         console.log(`Something went wrong and a message could not sent to the queue "${queueName}": ${err}`));
 });
+
+
+setInterval(() => {
+
+    const obj = {
+        dummyString: new Date().toISOString(),
+        result: "success"
+    };
+
+    producer.send(JSON.stringify(obj))
+    //.then(() => console.log("The producer sent a message to queue."))
+    .catch((err) =>
+        console.log(`Something went wrong and a message could not sent to the queue "${queueName}": ${err}`));
+
+}, 10);
