994c11b4c73dce6a67632fcaa1f3436b9f4ee66f
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-05-04 22:58:31 +0200

    split getQueues and renderQueues

--- src/monitor/monitor.ts
+++ src/monitor/monitor.ts
@@ -10,6 +10,8 @@ export default class Monitor extends BaseService {
     private config: IAppConfiguration;
     private monitorUpdateInterval: number;
     private timer: NodeJS.Timeout | undefined;
+    queues: string[];
+    visualizer: Visualizer;
 
     // private redis: Redis;
     // private redisSubscribedClient: Redis;
@@ -23,26 +25,37 @@ export default class Monitor extends BaseService {
             throw new Error("Configuration");
         }
 
+        this.queues = [];
         this.config = config;
         this.timer = undefined;
+        this.visualizer = new Visualizer();
         this.monitorUpdateInterval = config.monitorUpdateInterval;
     }
 
     async start() {
 
-        const visualizer = new Visualizer();
-        const queues = await this.repo.getQueues();
+        await this.getQueues();
+        await this.renderQueues();
 
-        setInterval(async () => {
-            const map = new Map<string, Metrics>();
-            for (let idx = 0; idx < queues.length; idx++) {
-                const key = queues[idx];
-                const result = await this.repo.getMetrics(key);
-                map.set(key, result);
-            }
-            const rows = RowsCreator.create(map);
-            visualizer.render(rows);
-        }, this.monitorUpdateInterval*1000);
+        //const getQueueInterval =
+        setInterval(async () => await this.getQueues(),
+            this.monitorUpdateInterval * 10 * 1000);
+
+        //const monitorUpdateInterval =
+        setInterval(async () => this.renderQueues(), this.monitorUpdateInterval * 1000);
+    }
+    async renderQueues(): Promise<void> {
+        const map = new Map<string, Metrics>();
+        for (let idx = 0; idx < this.queues.length; idx++) {
+            const key = this.queues[idx];
+            const result = await this.repo.getMetrics(key);
+            map.set(key, result);
+        }
+        const rows = RowsCreator.create(map);
+        this.visualizer.render(rows);
+    }
+
+    async getQueues(): Promise<void> {
+        this.queues = await this.repo.getQueues();
     }
 }
-
