f83bc50f3f0b45a3bc5cb6f0c8262d137eb58b33
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-05-04 22:57:19 +0200

    refactored Visualizer

--- src/monitor/visualizer.ts
+++ src/monitor/visualizer.ts
@@ -6,22 +6,26 @@ export default class Visualizer {
     timer: NodeJS.Timeout | undefined;
     size: number;
     cursor: number;
+    previousRowCount: number;
         constructor (size: number = 50) {
             this.size = size;
             this.cursor = 0
+            this.previousRowCount = 0;
         }
 
     render(rows: ReadonlyArray<Row>): void {
+        if (rows.length < this.previousRowCount) {
+            console.clear();
+        }
+        this.previousRowCount = rows.length;
+        this.cursor = 0
 
-        // disable the cursor
-        //process.stdout.write("\x1B[?25l")
-
-        // for (let i = 0; i < this.size; i++) {
-        //     process.stdout.write("\u2591")
-        // }
         for (let idx = 0; idx < rows.length; idx++) {
             const y = idx + 1;
             const row = rows[idx];
+
+            readline.cursorTo(process.stdout, 0, y);
+            process.stdout.clearLine(0);
 
             row.cells.forEach(elem => {
                 readline.cursorTo(process.stdout, elem.startIndex, y);
